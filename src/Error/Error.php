0704e65e7ec11d2fadfa26e652991f626075a1a1
ace411 <lochbm@live.com>
2018-12-14 16:29:35 +0300

    Converted value objects to simple data stores

--- src/Error/Error.php
+++ src/Error/Error.php
@@ -4,20 +4,5 @@ namespace Chemem\Bingo\Functional\Http\Error;
 
 class Error
 {
-    private $connErr;
-
-    public function __construct(string $connErr)
-    {
-        $this->connErr = $connErr;
-    }
-
-    public static function error(string $connErr) : Error
-    {
-        return new static($connErr);
-    }
-
-    public function get() : string
-    {
-        return $this->connErr;
-    }
+    public $connErr;
 }
--- src/Request/Request.php
+++ src/Request/Request.php
@@ -4,34 +4,11 @@ namespace Chemem\Bingo\Functional\Http\Request;
 
 class Request
 {
-    private $uri;
+    public $rqUri;
 
-    private $method;
+    public $rqMethod;
 
-    private $headers;
+    public $rqHeaders;
 
-    private $body;
-
-    public function __construct(string $uri, string $method, array $headers, array $body)
-    {
-        $this->uri = $uri;
-        $this->method = $method;
-        $this->headers = $headers;
-        $this->body = $body;
-    }
-
-    public static function request(string $uri, string $method, array $headers, array $body) : Request
-    {
-        return new self($uri, $method, $headers, $body);
-    }
-
-    public function get() : array
-    {
-        return array(
-            'rqURI' => $this->uri,
-            'rqMethod' => $this->method,
-            'rqHeaders' => $this->headers,
-            'rqBody' => $this->body
-        );
-    }
+    public $rqBody;
 }
